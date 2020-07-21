461e9ba63eacfd112694095767c084ac110d995c
amrit180 <62633596+amrit180@users.noreply.github.com>
2020-07-22 02:15:44 +0530

    Updates

---
+++ precache-manifest.154dd6b857bcd181785c7f93be7c0576.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e881131142dffdd317e34ce17ef97d3",
+    "url": "/react-app-link/index.html"
+  },
+  {
+    "revision": "f281957ace1971152041",
+    "url": "/react-app-link/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "acb5ddaafc750028c746",
+    "url": "/react-app-link/static/css/main.23eea72b.chunk.css"
+  },
+  {
+    "revision": "f281957ace1971152041",
+    "url": "/react-app-link/static/js/2.fbaa9f14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-link/static/js/2.fbaa9f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb5ddaafc750028c746",
+    "url": "/react-app-link/static/js/main.36461a6d.chunk.js"
+  },
+  {
+    "revision": "bc79e4b1228a3cc3f74e",
+    "url": "/react-app-link/static/js/runtime-main.9322e501.js"
+  },
+  {
+    "revision": "30d1addc1ad6606a889b378fb6d94995",
+    "url": "/react-app-link/static/media/4K_UHD_25fps_FREE_Video_Background___HUD_Sci_Fi_Plexus_Galactic_Map(720p).30d1addc.mp4"
+  },
+  {
+    "revision": "888c9dd459f036d6386ae725b78302c4",
+    "url": "/react-app-link/static/media/aboutus.888c9dd4.svg"
+  },
+  {
+    "revision": "c109d64eaa678cf1148110e51cc2c34a",
+    "url": "/react-app-link/static/media/bg.c109d64e.jpg"
+  },
+  {
+    "revision": "b49380af01cd810de01d227c5dd9773f",
+    "url": "/react-app-link/static/media/bili.b49380af.png"
+  },
+  {
+    "revision": "613cf57c00db5ba5c15b331620b6376f",
+    "url": "/react-app-link/static/media/nodejs.613cf57c.svg"
+  },
+  {
+    "revision": "856c0fc337bd46f3477c1a7636dabef7",
+    "url": "/react-app-link/static/media/undraw_Designer_by46.856c0fc3.svg"
+  },
+  {
+    "revision": "4a744e741d1d6bfc3b8536b11d6fcee6",
+    "url": "/react-app-link/static/media/undraw_complete_design_ongo.4a744e74.svg"
+  },
+  {
+    "revision": "fe97346df51091fc561f9ea5840cad85",
+    "url": "/react-app-link/static/media/undraw_design_tools_42tf.fe97346d.svg"
+  },
+  {
+    "revision": "e45563a3f856f132e5f08921c73553ea",
+    "url": "/react-app-link/static/media/undraw_hacker_mindset_gjwq.e45563a3.svg"
+  },
+  {
+    "revision": "295eb5468e9fefe65b4e1f1e5dcc08f5",
+    "url": "/react-app-link/static/media/undraw_pair_programming_njlp.295eb546.svg"
+  },
+  {
+    "revision": "09f88c592d4fdbb1d10d30f8f9362dc2",
+    "url": "/react-app-link/static/media/undraw_react_y7wq.09f88c59.svg"
+  },
+  {
+    "revision": "6c85616fb048350b4384a1a4ad8746cb",
+    "url": "/react-app-link/static/media/undraw_static_assets_rpm6.6c85616f.svg"
+  },
+  {
+    "revision": "b93c31f47703d06ba4b474b270f4a382",
+    "url": "/react-app-link/static/media/video.b93c31f4.mp4"
+  },
+  {
+    "revision": "7b8d9b1d6f30b4aea1977cda81e61daa",
+    "url": "/react-app-link/static/media/work.7b8d9b1d.svg"
+  }
+]);
